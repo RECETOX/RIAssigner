cce6fe2dd10b534f82faf194bc2031adc0e4b44a
hechth <helge.hecht@gmx.de>
2021-04-02 11:10:00 +0200

    Implemented function to find first index for rt

--- RIAssigner/data/PandasData.py
+++ RIAssigner/data/PandasData.py
@@ -1,13 +1,20 @@
 from .Data import Data
 from pandas import read_csv
+from ..utils import get_first_common_element
 
 
 class PandasData(Data):
     """ Class to handle data from filetypes which can be imported into a pandas dataframe. """
+    _column_names = set(['RT', 'rt', 'rts', 'retention_times', 'retention_time', 'retention', 'time'])
 
     def read(self, filename: str):
         self._data = read_csv(filename)
+        self._find_rt_index()
+
+    def _find_rt_index(self):
+        # Find first common element
+        self._index = get_first_common_element(self._data.columns, self._column_names)
 
     @property
     def retention_times(self):
-        return self._data['RT']
+        return self._data[self._index]

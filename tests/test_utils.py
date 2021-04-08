53a83add071589eb0b30b0a7372855909cf851cc
hechth <helge.hecht@gmx.de>
2021-04-08 17:27:23 +0200

    Fixed linter errors

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@ import pytest
 import RIAssigner.utils as Utils
 
 
-@pytest.mark.parametrize("first, second, expected", [[['blub', 'test', 'peter'], ['a', 'b', 'peter', 'd'], 'peter'], [['a'],['b'], None]])
+@pytest.mark.parametrize("first, second, expected", [[['blub', 'test', 'peter'], ['a', 'b', 'peter', 'd'], 'peter'], [['a'], ['b'], None]])
 def test_get_first_common_element(first, second, expected):
     actual = Utils.get_first_common_element(first, second)
 

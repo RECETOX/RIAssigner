83091cb702ee166ff3f810806713296bbdb3cf8e
hechth <helge.hecht@gmx.de>
2021-04-20 09:25:39 +0200

    Reorganized code and added documentation

--- RIAssigner/data/MatchMSData.py
+++ RIAssigner/data/MatchMSData.py
@@ -15,9 +15,6 @@ class MatchMSData(Data):
         self._read_retention_times()
         self._read_retention_indices()
 
-    def _read_retention_indices(self):
-        self._retention_indices = [safe_read_key(spectrum, 'retentionindex') for spectrum in self._spectra]
-
     def _read_spectra(self, filename):
         if filename.endswith('.msp'):
             self._spectra = list(load_from_msp(filename))
@@ -25,14 +22,21 @@ class MatchMSData(Data):
             raise NotImplementedError("Currently only supports 'msp'.")
 
     def _read_retention_times(self):
+        """ Read retention times from spectrum metadata. """
         self._retention_times = [safe_read_key(spectrum, 'retentiontime') for spectrum in self._spectra]
+
+    def _read_retention_indices(self):
+        """ Read retention indices from spectrum metadata. """
+        self._retention_indices = [safe_read_key(spectrum, 'retentionindex') for spectrum in self._spectra]
 
     @property
     def retention_times(self) -> Iterable[Data.RetentionTimeType]:
+        """ Get retention times. """
         return self._retention_times
 
     @property
     def retention_indices(self) -> Iterable[Data.RetentionIndexType]:
+        """ Get retention indices."""
         return self._retention_indices
 
     @retention_indices.setter
@@ -41,6 +45,22 @@ class MatchMSData(Data):
 
 
 def safe_read_key(spectrum: Spectrum, key: str) -> Optional[float]:
+    """ Read key from spectrum and convert to float or return 'None'.
+    Tries to read the given key from the spectrum metadata and convert it to a float.
+    In case an exception is thrown or the key is not present, returns 'None'.
+
+    Parameters
+    ----------
+    spectrum:
+        Spectrum from which to read the key.
+    key:
+        Key to be read from the spectrum metadata.
+
+    Returns
+    -------
+        Either the key's value converted to float or 'None'.
+    """
+
     value = spectrum.get(key, default=None)
     if value is not None:
         try:

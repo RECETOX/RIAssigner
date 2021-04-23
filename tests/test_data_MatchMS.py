ff8f496ab4a4724e574dd48f09ef3eadee079bb1
maximskorik <maximskoryk@gmail.com>
2021-04-23 11:20:11 +0200

    simplify retention_times()

--- tests/test_data_MatchMS.py
+++ tests/test_data_MatchMS.py
@@ -23,16 +23,14 @@ def retention_times(filename_msp):
     retention_times = []
     for spectrum in library:
         rt = spectrum.get('retentiontime', None)
-        if rt == '':
-            rt = 0.0
-        elif isinstance(rt, str):
+        if isinstance(rt, str):
             try:
                 rt = float(rt)
             except ValueError:
                 rt = ''
         retention_times.append(rt)
     retention_times.sort()
-    retention_times = [None if (i=='' or i==0.0) else i for i in retention_times]
+    retention_times = [None if i=='' else i for i in retention_times]
     return retention_times
 
 

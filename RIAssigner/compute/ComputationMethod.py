c5617a472c741e1521e9758d3cc3d057be579c96
hechth <helge.hecht@gmx.de>
2021-04-27 17:58:33 +0200

    Added method to check data args

--- RIAssigner/compute/ComputationMethod.py
+++ RIAssigner/compute/ComputationMethod.py
@@ -8,3 +8,7 @@ class ComputationMethod(ABC):
     @abstractmethod
     def compute(self, query: Data, reference: Data) -> List[float]:
         ...
+
+    def _check_data_args(self, query, reference):
+        assert query is not None, "Query data is 'None'."
+        assert reference is not None, "Reference data is 'None'."

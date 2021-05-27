4cffe94fe1057bed66876a6d89b348a8c5bb94c4
maximskorik <maximskoryk@gmail.com>
2021-05-27 15:24:04 +0200

    add test of define_separator

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,3 +7,10 @@ def test_get_first_common_element(first, second, expected):
     actual = Utils.get_first_common_element(first, second)
 
     assert actual == expected
+
+
+@pytest.mark.parametrize("filename, expected", [('test_file.csv', ','), ('test_file.tsv', '\t')])
+def test_define_separator(filename, expected):
+    actual = Utils.define_separator(filename)
+
+    assert actual == expected

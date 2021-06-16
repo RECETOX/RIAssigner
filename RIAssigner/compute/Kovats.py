ad7f7a3eb1a0236e8a8136651f002ac50f482c15
hechth <helge.hecht@recetox.muni.cz>
2021-06-16 15:14:18 +0200

    Small refactorings and renamings, as well as documentation

--- RIAssigner/compute/Kovats.py
+++ RIAssigner/compute/Kovats.py
@@ -6,7 +6,7 @@ from .ComputationMethod import ComputationMethod
 class Kovats(ComputationMethod):
     """ Class to compute the Kovats retention index. """
 
-    def compute(self, query: Data, reference: Data) -> List[float]:
+    def compute(self, query: Data, reference: Data) -> List[Data.RetentionIndexType]:
         """ Compute non-isothermal Kovats retention index.
         For details see https://webbook.nist.gov/chemistry/gc-ri/.
 
@@ -19,26 +19,44 @@ class Kovats(ComputationMethod):
         self._check_data_args(query, reference)
 
         index = 0
-        retention_indices = []
 
         # Copy rts and ris and insert 0 in the beginning, so that interpolation always starts at 0,0 to the first reference compound.
-        reference_rts = list(reference.retention_times)
-        reference_ris = list(reference.retention_indices)
+        reference_rts = [0.0] + list(reference.retention_times)
+        reference_ris = [0.0] + list(reference.retention_indices)
 
-        reference_rts.insert(0, 0.0)
-        reference_ris.insert(0, 0.0)
-
-        for target_rt in query.retention_times:
-            ri = None
-            if Data.is_valid(target_rt):
-                index = _get_index(target_rt, reference_rts, index)
-                ri = _compute_ri(target_rt, reference_rts, reference_ris, index)
-            retention_indices.append(ri)
+        retention_indices = [
+            self._compute_ri(target_rt, reference_rts, reference_ris, index)
+            for target_rt in query.retention_times
+        ]
 
         return retention_indices
 
+    def _compute_ri(self,
+                    target_rt: Data.RetentionTimeType,
+                    reference_rts: Iterable[Data.RetentionTimeType],
+                    reference_ris: Iterable[Data.RetentionTimeType],
+                    index: int) -> Data.RetentionIndexType:
+        """Compute retention index for target retention time.
 
-def _get_index(target_rt: float, reference_rts: Iterable[Data.RetentionTimeType], index: int):
+        Args:
+            target_rt (Data.RetentionTimeType): Retention time for which to compute the index
+            reference_rts (Iterable[Data.RetentionTimeType]): Reference retention times
+            reference_ris (Iterable[Data.RetentionTimeType]): Reference retention indices
+            index (int): Current reference index
+
+        Returns:
+            Data.RetentionIndexType: Computed retention index
+        """
+
+        ri = None
+        if Data.is_valid(target_rt):
+            index = _update_index(target_rt, reference_rts, index)
+            ri = _compute_kovats(target_rt, reference_rts,
+                                 reference_ris, index)
+        return ri
+
+
+def _update_index(target_rt: float, reference_rts: Iterable[Data.RetentionTimeType], index: int):
     """ Get the indices of previosly eluting and next eluting reference compounds.
     Retention times in 'Data' objects are sorted in ascending order, so this method assumes
     that 'reference_rt' is sorted in ascending order.
@@ -57,7 +75,7 @@ def _get_index(target_rt: float, reference_rts: Iterable[Data.RetentionTimeType]
     return index
 
 
-def _compute_ri(
+def _compute_kovats(
         target_rt: float,
         reference_rts: Iterable[Data.RetentionTimeType],
         reference_ris: Iterable[Data.RetentionIndexType],

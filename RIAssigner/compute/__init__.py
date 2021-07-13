dc4dd5606108731ce4f19956043a273034ef64bc
hechth <helge.hecht@recetox.muni.cz>
2021-07-13 16:35:54 +0200

    Added ComputationMethod class to package

--- RIAssigner/compute/__init__.py
+++ RIAssigner/compute/__init__.py
@@ -1,11 +1,13 @@
 import logging
 
+from .ComputationMethod import ComputationMethod
 from .CubicSpline import CubicSpline
 from .Kovats import Kovats
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __all__ = [
+    "ComputationMethod",
     "CubicSpline",
     "Kovats",
 ]

255109000aa4506d38ba144237c4a5003d8b3cca
hechth <helge.hecht@recetox.muni.cz>
2021-07-13 13:41:09 +0200

    Implemented first version of CLI

---
+++ RIAssigner/__main__.py
@@ -0,0 +1,50 @@
+from RIAssigner.compute.ComputationMethod import ComputationMethod
+import argparse
+
+from RIAssigner.cli import CreateMethodAction, LoadDataAction
+from RIAssigner.data import Data
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(prog="RIAssigner")
+    parser.add_argument("reference",
+                        type=str,
+                        action=LoadDataAction,
+                        help="Reference dataset containing retention times and indices. Path to CSV or MSP.")
+    parser.add_argument("query",
+                        type=str,
+                        action=LoadDataAction,
+                        help="Query dataset for which to compute retention indices. Path to CSV or MSP.")
+    parser.add_argument("method",
+                        type=str,
+                        action=CreateMethodAction,
+                        choices=['kovats', 'cubiscpline'],
+                        help="Computation method for RI.")
+    parser.add_argument("output",
+                        type=str,
+                        help="Output filename.")
+    return parser
+
+
+def main():
+    """Command line interface for the RIAssigner library.
+
+    Args:
+        argv (List[string]): Arguments passed to the program
+    """
+    parser = create_parser()
+    args = parser.parse_args()
+
+    query: Data = args.query
+    reference: Data = args.reference
+    method: ComputationMethod = args.method
+
+    retention_indices = method.compute(query, reference)
+    query.retention_indices = retention_indices
+
+    query.write(args.output)
+    return 0
+
+
+if __name__ == "__main__":
+    main()

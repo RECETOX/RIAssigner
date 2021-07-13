a249279df2df7865df1aae584857357f1e43d975
hechth <helge.hecht@recetox.muni.cz>
2021-07-13 11:16:10 +0200

    Implemented basic test for CLI action with simple comparison

--- tests/test_cli_LoadDataAction.py
+++ tests/test_cli_LoadDataAction.py
@@ -1,10 +1,10 @@
-from RIAssigner.data.PandasData import PandasData
 import argparse
 import os.path
-import pytest
 
+import numpy
 from RIAssigner.cli import LoadDataAction
-from RIAssigner.data import MatchMSData
+from RIAssigner.data import PandasData
+
 from tests.fixtures.data import data_location
 
 
@@ -14,12 +14,18 @@ def test_create():
 
 
 def test_load_data_pandas():
+    # Arrange
     filename = os.path.join(data_location, "csv", "Alkanes_20210325.csv")
     expected = PandasData(filename)
     namespace = argparse.Namespace()
     parser = argparse.ArgumentParser()
     sut = LoadDataAction("", "data")
 
+    # Act
     sut(parser, namespace, filename)
     actual = namespace.data
-    assert actual == expected
+
+    # Assert
+    # TODO: Replace with proper comparison operator once implemented
+    numpy.testing.assert_array_almost_equal(actual.retention_times, expected.retention_times)
+    numpy.testing.assert_array_almost_equal(actual.retention_indices, expected.retention_indices)

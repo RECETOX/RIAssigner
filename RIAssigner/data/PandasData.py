509c58e181dd2cbe6b6c2569476e9a9bddd9dac6
hechth <helge.hecht@recetox.muni.cz>
2022-01-27 10:32:08 +0100

    Fixed typo in exception text

--- RIAssigner/data/PandasData.py
+++ RIAssigner/data/PandasData.py
@@ -29,7 +29,7 @@ class PandasData(Data):
 
     def write(self, filename: str):
         """ Write data on disk. Currently supports 'csv' and 'tsv' formats. """
-        assert filename.endswith((".csv", ".tsv")), "File extention must be 'csv' or 'tsv'."
+        assert filename.endswith((".csv", ".tsv")), "File extension must be 'csv' or 'tsv'."
         separator = define_separator(filename)
         self._data.to_csv(filename, index=False, sep=separator)
 

902d95583d2b4aaf7a93c00496765d1c927ba929
hechth <helge.hecht@recetox.muni.cz>
2022-02-11 10:02:10 +0100

    Finalized tests

--- tests/test_data_SimpleData.py
+++ tests/test_data_SimpleData.py
@@ -1,11 +1,7 @@
 import numpy as np
 import pytest
-from RIAssigner.data import SimpleData
+from tests.builders import SimpleDataBuilder
 
-
-def test_constructor():
-    actual = SimpleData([0.0, 12.0], "sec")
-    assert actual is not None
 
 @pytest.mark.parametrize("retention_times, retention_indices, expected", [
     [[None, 45.9], None,"Invalid retention time data."], # contains None
@@ -15,9 +11,10 @@ def test_constructor():
     [[12.0, 100.9], [100], "Retention time and index data are of different length."],
     [[12.0, 100.9], [200, 100], "Retention index data has to be sorted."]
 ])
-def test_exception_invalid_rt(retention_times, retention_indices, expected):
+def test_constructor_exception(retention_times, retention_indices, expected):
     with pytest.raises(AssertionError) as exception:
-        SimpleData(retention_times, "sec", retention_indices)
+        builder = SimpleDataBuilder().with_rt(retention_times).with_ri(retention_indices)
+        builder.build()
 
     message = exception.value.args[0]
     assert message == expected, message
@@ -28,14 +25,16 @@ def test_exception_invalid_rt(retention_times, retention_indices, expected):
     [[0, 0.5], "min", [0, 30.0]]
 ])
 def test_get_retention_times(retention_times, rt_unit, expected):
-    sut = SimpleData(retention_times, rt_unit)
+    builder = SimpleDataBuilder().with_rt(retention_times).with_rt_unit(rt_unit)
+    sut = builder.build()
     actual = sut.retention_times
 
     np.testing.assert_array_equal(actual, expected)
 
 def test_get_retention_indices():
     expected = [134.5, 245.56, 789]
-    sut = SimpleData([100, 200 ,300], "sec", expected)
+    builder = SimpleDataBuilder().with_rt([100, 200 ,300]).with_ri(expected).with_rt_unit("sec")
+    sut = builder.build()
     actual = sut.retention_indices
     assert actual == expected
     

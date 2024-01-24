93884de7df0289b26578925fb9e95c3b7c75555c
wverastegui <wudmir@gmail.com>
2024-01-24 15:24:54 +0100

    added test case for UnitRegistry

--- tests/test_data_Data.py
+++ tests/test_data_Data.py
@@ -3,6 +3,8 @@ import os
 import pytest
 from RIAssigner.data import Data
 
+from pint import UnitRegistry, Quantity
+
 here = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -13,3 +15,27 @@ def test_abc():
     message = exception.value.args[0]
     assert exception.typename == "TypeError"
     assert str(message).startswith("Can't instantiate abstract class Data with abstract methods")
+
+#TODO: first test simple value and unit, then test array and unit, then test None and unit, None and None, Array of None and None, mixed array and unit
+@pytest.mark.parametrize("magnitude, unit", [
+    (1.0, "sec"),
+    ([1.0, 2.0, 3.0], "sec"),
+    (1.0, None),
+    ([None, None, None], None),
+    ([1.0, 2.0, 3.0], None),
+    ([1.0, None, None], "sec"),
+    ([1.0, None, 3.0], None)
+  ])
+def test_uregistry(magnitude, unit):
+    ureg = UnitRegistry().Quantity(magnitude, unit)
+    assert isinstance(ureg, Quantity)
+
+@pytest.mark.parametrize("magnitude, unit", [
+    (None, "sec"),
+    (None, None)
+  ])
+def test_uregistry_exceptions(magnitude, unit):
+    with pytest.raises(TypeError) as exception:
+        UnitRegistry().Quantity(magnitude, unit)
+
+    assert exception.typename == "TypeError"
